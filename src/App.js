63fd403ce74223341228c2912d2651e5bf40cc33
Nelson Ogueh <nelsonovovwe@gmail.com>
2022-01-29 11:21:58 +0100

    Feat: Header initialized

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';s
+import "./App.css";
+
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome to Helping Hand</h2>
       </header>
     </div>
   );
