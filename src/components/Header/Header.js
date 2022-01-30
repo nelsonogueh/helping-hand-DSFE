e6f88c05feee6253ed087c7a6a002892cda75f98
Nelson Ogueh <nelsonovovwe@gmail.com>
2022-01-30 06:24:47 +0100

    Landing page - section 1 completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,46 @@
-import React  from "react";
-import {
-  Navbar,
-  Container,
-  Nav
-} from "react-bootstrap";
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 import './Header.css'
 import logo from '../../assets/img/logo.png'
 
-
 const Header = (props) => {
   return (
-    <Navbar className="navbar-main-container" bg="light" expand="lg"> 
+    <Navbar className="navbar-main-container sticky" bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#"><img alt="" src={logo} style={{height: '50px'}} /></Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img alt="" src={logo} style={{ height: '50px' }} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            style={{ maxHeight: '100px' }}
             navbarScroll
-          > 
-            <Nav.Link className="menu-nav-item" href="#action1">HOME</Nav.Link>
-            <Nav.Link className="menu-nav-item" href="#action2">CONTRIBUTE</Nav.Link>
-            <Nav.Link className="menu-nav-item" href="#action2">ABOUT US</Nav.Link>
-            <Nav.Link className="menu-nav-item" href="#action2">FAQs</Nav.Link>
+          >
+            <Nav.Link className="menu-nav-item" href="#action1">
+              HOME
+            </Nav.Link>
+            <Nav.Link className="menu-nav-item" href="#action2">
+              CONTRIBUTE
+            </Nav.Link>
+            <Nav.Link className="menu-nav-item" href="#action2">
+              ABOUT US
+            </Nav.Link>
+            <Nav.Link className="menu-nav-item" href="#action2">
+              FAQs
+            </Nav.Link>
           </Nav>
-          <Nav.Link href="#login" className="header-btn primary-button-hollow">LOGIN</Nav.Link>
-          <Nav.Link href="#login" className="header-btn primary-button">SIGN UP</Nav.Link>
+          <Nav.Link href="#login" className="header-btn primary-button-hollow">
+            LOGIN
+          </Nav.Link>
+          <Nav.Link href="#login" className="header-btn primary-button">
+            SIGN UP
+          </Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,26 +1,35 @@
-import React  from "react";
-import {
-  Navbar,
-  Container,
-  Nav
-} from "react-bootstrap";
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 import './LandingPage.css'
-import bgDesign from '../../assets/img/bg-design.png'
+import bgDesign from '../../assets/img/bg-design.jpg'
 import bgDesignSmall from '../../assets/img/bg-design-small.png'
 import donation from '../../assets/img/donation.png'
 import image from '../../assets/img/image.png'
-
-
+import bgEllipse from '../../assets/img/bg-ellipse.png'
 
 const LandingPage = (props) => {
   return (
-    <section className="section1-container">
-        <div><h1>What are you in NEED of?</h1>
-<h1>Request for it and it shall be given to you </h1>
-<button  className="primary-button">Request </button></div>
-    </section>
-  );
-};
+    <div>
+      <section
+        className="section1-container"
+        // style={{ backgroundImage: `url(${bgEllipse})` }}
+      >
+        <div className="section1-content-container">
+          <h1 className="section1-content-h1">
+            What are you in <span>NEED</span> of?
+          </h1>
+          <h1 className="section1-content-h2">
+            Request for it and it shall be given to you{' '}
+          </h1>
+          <button className="primary-button section1-primary-button ">
+            REQUEST{' '}
+          </button>
+        </div>
+      </section>
+      <section className="design-section1"></section>
+    </div>
+  )
+}
 
-export default LandingPage;
+export default LandingPage
