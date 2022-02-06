ec016ca6e0a3acc87af26429431085da573ce43a
Nelson Ogueh <nelsonovovwe@gmail.com>
2022-02-06 15:24:46 +0100

    Dashboard started

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignedDonation from './components/signed-in/SignedDonation/SignedDonation
 import Dashboard from './components/signed-in/Dashboard/Dashboard'
 import SigninContainer from './components/container/sign-up-sign-incontainer/SigninContainer'
 import SignIn from './components/SignIn/SignIn'
+import Blank from './components/signed-in/Blank/Blank'
 // import SignSideBar from './components/container/sign-up-sign-incontainer/SignSideBar/SignSideBar'
 
 // import SignInContent from './components/container/SignInContent/SignInContent'
@@ -47,6 +48,14 @@ function App() {
             }
           />
           <Route
+            path="/login"
+            element={
+              <SigninContainer>
+                <SignIn />
+              </SigninContainer>
+            }
+          />
+          <Route
             path="/dashboard"
             element={
               <ContainerSigned>
@@ -62,14 +71,14 @@ function App() {
               </ContainerSigned>
             }
           />
-
-          {/* <Route path="/login" element={<SignSideBar />} /> */}
+          {/* BLANK COMPONENT AS A TEMPLATE FOR CREATING SIGNED IN COMPONENTS */}
+          {/* BLANK COMPONENT AS A TEMPLATE FOR CREATING SIGNED IN COMPONENTS */}
           <Route
-            path="/login"
+            path="/blank"
             element={
-              <SigninContainer>
-                <SignIn />
-              </SigninContainer>
+              <ContainerSigned>
+                <Blank />
+              </ContainerSigned>
             }
           />
         </Routes>
--- src/components/signed-in/Dashboard/Dashboard.js
+++ src/components/signed-in/Dashboard/Dashboard.js
@@ -3,13 +3,45 @@ import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 
 import './Dashboard.css'
 
-const Dashboard = (props) => {
+const Blank = (props) => {
   return (
     <div>
-      <Container fluid className="donation-main-container">
+      <Container fluid className="content-main-container">
         <Row>
           <Col>
-            <h1>Dashboard</h1>
+            <div className="dashboard-container">
+              <div className="dashboard-report-container">
+                <div className="dashboard-summary-item">
+                  <div className="dashboard-no-request-container">
+                    No. of request per month
+                  </div>
+                  <div className="dashboard-summary-counter-text">5</div>
+                  <div className="dashboard-view-detail-link">
+                    <a href="#">View details</a>
+                  </div>
+                </div>
+                <hr className="dashboard-summary-hr" />
+                <div className="dashboard-summary-item">
+                  <div className="dashboard-no-request-container">
+                    Total items requested
+                  </div>
+                  <div className="dashboard-summary-counter-text">3</div>
+                  <div className="dashboard-view-detail-link">
+                    <a href="#">View details</a>
+                  </div>
+                </div>
+                <hr className="dashboard-summary-hr" />
+                <div className="dashboard-summary-item">
+                  <div className="dashboard-no-request-container">
+                    Total items donated
+                  </div>
+                  <div className="dashboard-summary-counter-text">0</div>
+                  <div className="dashboard-view-detail-link">
+                    <a href="#">View details</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
@@ -17,4 +49,4 @@ const Dashboard = (props) => {
   )
 }
 
-export default Dashboard
+export default Blank
