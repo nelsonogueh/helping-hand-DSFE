098e662685076b55ee392890bda217eecd5e9a4f
Nelson <nelsonovovwe@gmail.com>
2022-02-08 21:46:26 +0100

    Fixes

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 
 import './NotFound.css'
-import donation from '../../assets/img/donation.png'
+import notfoundImg from '../../assets/img/not-found.gif'
 
 const NotFound = (props) => {
   document.title = 'ERROR 404 - NOT FOUND | DAR'
@@ -12,8 +12,10 @@ const NotFound = (props) => {
         <Row>
           <Col>
             {' '}
-            <section className="section1-container">
+            <section className="notfound-section-container">
               <div className="section1-content-container">
+                <div></div>
+                <img className='notfound-img' src={notfoundImg} />
                 <h1 className="section1-content-h1">404 NOT FOUND</h1>
                 <div className="section1-content-h2 notfound-sub-text">
                   Sorry! The content you're looking for could not be found.
